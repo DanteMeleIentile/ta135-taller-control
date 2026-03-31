665cde475a81a1f1da29b74d86aadf29007fa40f
Dante Mele Ientile <dantemeleientile@gmail.com>
2026-03-31 20:44:48 -0300

    primera parte ok

---
+++ lectura_50hz/lectura_50hz.ino
@@ -0,0 +1,60 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+unsigned long t_anterior = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+  delay(100);
+}
+
+
+void loop() {
+  unsigned long t_actual = micros();
+
+
+
+
+
+  if ((t_actual - t_anterior) >= (20000)) {    
+    t_anterior = t_actual;
+    /* ***************** */
+    
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+  
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+  
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+    Serial.println("");
+
+  }
+
+}
