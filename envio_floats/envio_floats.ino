5383726ee764ea551246797499c529a14caf0479
Dante Mele Ientile <dantemeleientile@gmail.com>
2026-04-13 20:27:15 -0300

    envio floats funcionando

--- envio_floats/envio_floats.ino
+++ envio_floats/envio_floats.ino
@@ -1,23 +1,39 @@
+#define TIPO_TEST 1 // 0 -> write; 1 -> flush 
+
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+  delay(2000);
+
   float dato = 1.11; 
   byte* a_enviar = (byte*)&dato;
+
+  #if TIPO_TEST == 0
+    Serial.println()
+    Serial.println("--- ENVIO CON WRITE ---");
+  #elif TIPO_TEST == 1
+    Serial.println()
+    Serial.println("--- ENVIO CON FLUSH ---");
+  #endif
+
+  delay(1000);
+
 
   unsigned long t_start = micros();
 
   for (int i = 0; i < 100; i++) {
     Serial.write(a_enviar, 4); 
   }
-
+  
+  #if TIPO_TEST == 1
+    Serial.flush(); //se queda esperando que se vacíe el buffer
+  #endif
+ 
   unsigned long t_finish = micros();
 
-  Serial.println("\n--- RESULTADO DEL EXPERIMENTO ---");
-  Serial.print("Tiempo total para 100 floats: ");
+  Serial.println();
+  Serial.print("Tiempo TOTAL: ");
   Serial.print(t_finish - t_start);
-  Serial.println(" microsegundos");
 }
 
 void loop() {
-  // Queda vacío, el experimento se hace una sola vez en el setup
 }
