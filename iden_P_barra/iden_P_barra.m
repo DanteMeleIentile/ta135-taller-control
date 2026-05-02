cb44417a0417a2766e2d93393b23e59f56a1cead
Dante Mele Ientile <dantemeleientile@gmail.com>
2026-05-02 20:35:02 -0300

    test 2 ident p_barra

--- iden_P_barra/iden_P_barra.m
+++ iden_P_barra/iden_P_barra.m
@@ -1,28 +1,29 @@
-%save('nombre.mat')
+%save('datosX.mat', 'raw_ang_X', 'raw_u_X', 'raw_dist_X', 'indices_X', 't_v_X', 'ang_X', 'u_X', 'dist_X');
 close all;clc
 clearvars -except out
 
 
-t_inicial   = 0.5;
-t_final     = 1.5;
+t_inicial   = 0.7;
+t_final     = 1.2;
 Ts          = 20e-3;
 
 %obtengo los datos
-t_real_1        = out.tout;
-raw_ang_1       = out.angle_barra;
-raw_u_1         = out.servo; 
-raw_dist_1      = out.dist;
+t_real_2        = out.tout;
+raw_ang_2       = out.angle_barra;
+raw_u_2         = out.servo; 
+raw_dist_2      = out.dist;
 
-indices = (t_real_1 >= t_inicial) & (t_real_1 <= t_final);
-t_v_1   = t_real_1(indices);
-ang_1   = raw_ang_1(indices);
-u_1     = raw_u_1(indices);
-dist_1  = raw_dist_1(indices);
+indices_2 = (t_real_2 >= t_inicial) & (t_real_2 <= t_final);
+indices = indices_2;
+t_v_2   = t_real_2(indices);
+ang_2   = raw_ang_2(indices);
+u_2     = raw_u_2(indices);
+dist_2  = raw_dist_2(indices);
 
 figure;
-plot(t_v_1, ang_1, 'b', 'LineWidth', 1.2); 
+plot(t_v_2, ang_2, 'b', 'LineWidth', 1.2); 
 hold on; 
-plot(t_v_1, u_1 * 0.03, 'g', 'LineWidth', 1.5); 
+plot(t_v_2, u_2 * 0.03, 'g', 'LineWidth', 1.5); 
 grid on;
 title("Ventana de tiempo seleccionada");
 hold off;
@@ -30,8 +31,8 @@ hold off;
 
 %%
 % Ecuación: y(n+2) = a1*y(n+1) + a2*y(n) + b1*u(n)
-Y_obs = ang_1(3:end); 
-X_reg = [ang_1(2:end-1), ang_1(1:end-2), u_1(1:end-2)]; % retraso de 2 muestras
+Y_obs = ang_2(3:end); 
+X_reg = [ang_2(2:end-1), ang_2(1:end-2), u_2(1:end-2)]; % retraso de 2 muestras
 % lo que hace es poner un cero de fase no minimaen continua para simular el
 % restraso de 2 muestras
 
@@ -48,27 +49,27 @@ Hz = tf(num_z, den_z, Ts);
 
 Hs = d2c(Hz, 'tustin');
 s=tf('s');
-p1  = -22
-p2  = -23
-k_c = +0.04133;
+p1  = -23
+p2  = -22
+k_c = +0.03897; %ang_x / u_x
 Hs = p1*p2*k_c /((s-p1)*(s-p2));
 
 polos_z = roots(den_z)
 polos_s = pole(Hs)
 
 % Simulamos la respuesta del modelo con la entrada real
-y_sim_z = lsim(Hz, u_1, t_v_1);
-y_sim_s = lsim(Hs, u_1, t_v_1);
+y_sim_z = lsim(Hz, u_2, t_v_2);
+y_sim_s = lsim(Hs, u_2, t_v_2);
 
-error = ang_1 - y_sim_s;
+error = ang_2 - y_sim_s;
 MSE = mean(error.^2);
 fprintf('\nMSE en ventana: %.4f\n', MSE);
 
 
 figure();
-plot(t_v_1, ang_1, 'g', 'LineWidth', 1.5); hold on;
-plot(t_v_1, y_sim_s, 'y--', 'LineWidth', 1.5);
-plot(t_v_1, u_1 * 0.03, 'b', 'LineWidth', 1); 
+plot(t_v_2, ang_2, 'g', 'LineWidth', 1.5); hold on;
+plot(t_v_2, y_sim_s, 'y--', 'LineWidth', 1.5);
+plot(t_v_2, u_2 * 0.03, 'b', 'LineWidth', 1); 
 title('Planta Real vs. Modelo Identificado');
 legend('Real', 'Estimación', 'Entrada (u)');
 grid on;
