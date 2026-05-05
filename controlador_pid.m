5613691dfc64c5632cd9c189f6ea586433189240
Gonzalo Antahuara <gantahuara@fi.uba.ar>
2026-05-05 19:54:25 -0300

    Presentacion

--- controlador_pid.m
+++ controlador_pid.m
@@ -3,11 +3,11 @@ s=tf('s');
 
 Ts = 20e-3;
 
+% Definicion de la Planta 
 k_planta = -0.099863;
 p_planta = 10.14;
 P = k_planta * s/( (s+p_planta) * (s-p_planta) )
 
-% Forma Canónica Controlable
 p1_cuad = 10.14^2;
 A = [0, 1 ; 
      p1_cuad, 0];
@@ -21,18 +21,13 @@ sys_fisc = ss(A, B, C, D);
 x1_0 = 5 * pi / (180);
 x2_0 = 0;
 x0 = [x1_0; x2_0];
-%initial(sys_fisc, x0);
 
-
-
-% ----- DISEÑO CONTROLADOR ----- %
+% ----- CONTROLADOR DISEÑO POR LOOP SHAPING----- %
 kc = -1 * db2mag(56.5);
 C = zpk([-11, -12],[0, -80], kc)
 pid(C)
 
 L = minreal(C*P);
-
-%%
 
 % ----- GRAFICOS ----- %
 optionss=bodeoptions;
@@ -42,34 +37,26 @@ optionss.PhaseMatchingValue=-180;
 optionss.PhaseMatchingFreq=1;
 optionss.Grid='on';
 
-
-%figure();
-%bode(P,optionss);title("P")
-
-%figure();
-%bode(C,optionss);title("C")
-
 figure();
 bode(L,optionss);title("L")
 
-%%
-% ----- CONTROL DIGITAL ----- %
-c_dig = c2d(C, Ts, 'tustin')
+% ---------- AJUSTE MANUAL DE GANANANCIAS DEL PId = Kp + Ki * 1/s + Kd * s;
 
-%pid = Kp + Ki * 1/s + Kd * s;
-
-%0.004 * ki
-% con esta configuracion el controlador satura 
-% acciones de control
-% derivatvo = 25 , integrak = 5, proporcional = 10
-
-% $$u[k] = u[k-1] - 694.94 e[k] + 567.00 e[k-1]$$
-
-%%
 k_s = pid(C)
 C_pid = (k_s.Kp - 300) + (k_s.Ki+300) /s + (k_s.Kd+3)*s/(k_s.Tf * s+1 )
 
+% ----- CONTROL DIGITAL (para simulink)----- %
 c_dig = c2d(C_pid, Ts, 'tustin')
 simu = pid(C_pid)
 
-% últ probado en clase (vídeo): %C_pid = (-316) + (-2100) /s + (0)*s/(k_s.Tf * s+1 ) 
+% últ probado en clase 28/04/26: %C_pid = (-316) + (-2100) /s + (0)*s/(k_s.Tf * s+1 ) 
+
+%% POSIBLES RESPUESTAS 
+% 
+% Caso 1) Luego de un rato acelera y cae 
+% 	Diagnostico: se acumulo error y el Ki elevado hizo que el error sea mayor
+% 	Respuesta: Bajar el Ki
+% 
+% Caso 2) Si la estabilización es muy variable
+% 	Diagnostico: Kd es muy grande y no se asjuta de forma estable
+% 	Respuesta: Bajar el Kd
