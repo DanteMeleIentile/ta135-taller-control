bf50dd051f912832a81148520d4425b0f41ace1c
Gonzalo Antahuara <gantahuara@fi.uba.ar>
2026-05-04 21:34:00 -0300

    Contrlador proporcional

--- controlador.m
+++ controlador.m
@@ -1,6 +1,13 @@
 close all;clc
 clearvars -except out
 
+optionss=bodeoptions;
+optionss.MagVisible='on';
+optionss.PhaseMatching='on';
+optionss.PhaseMatchingValue=-180;
+optionss.PhaseMatchingFreq=1;
+optionss.Grid='on';
+
 t_inicial       = 0;
 t_final         = 5;
 Ts              = 20e-3;
@@ -41,20 +48,28 @@ H_carrito = p3*p4*k_c /((s-p3)*(s-p4));
 
 H_total = H_barra * H_carrito
 
-figure();
-rlocus(H_total);
+k = db2mag(36.5);
 
-dist_simulada_total = lsim(H_total, u_1, t_v_1);
+C_k = k;
+
+L = C_k * H_total;
+
+figure();
+bode(H_total);
+
+figure();
+bode(L);
+
+dist_simulada_total = lsim(L, u_1, t_v_1);
 
 figure;
-plot(t_v_1, (dist_1 - 4), 'g', 'LineWidth', 1.5); hold on;
-plot(t_v_1, dist_simulada_total , 'r--', 'LineWidth', 1.5); hold on;
-plot(t_v_1, u_1 , 'b', 'LineWidth', 1.5); 
+plot(t_v_1, dist_1 - 4, 'g', 'LineWidth', 1.5); hold on;
+plot(t_v_1, dist_simulada_total, 'r--', 'LineWidth', 1.5);
+plot(t_v_1, u_1, 'b--', 'LineWidth', 1.5);
+ylim([0,25]);
 title('Planta Completa (4 Polos): Distancia Real vs Simulada');
 xlabel('Tiempo [s]');
 ylabel('Distancia [m]');
-ylim([-5, 20]);
-xlim([0, 2.5]);
 legend('Distancia Real (Sensor)', 'Distancia Simulada (Modelo)');
 grid on;
 hold off;
