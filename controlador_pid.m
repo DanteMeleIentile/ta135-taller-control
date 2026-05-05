74ed9357370a951c8c76b17b41277169f3348c67
Dante Mele Ientile <dantemeleientile@gmail.com>
2026-05-04 21:17:23 -0300

    Check 4_5_26

--- controlador_pid.m
+++ controlador_pid.m
@@ -1,16 +1,6 @@
 clear all;close all;clc
 s=tf('s');
 
-
-optionss=bodeoptions;
-optionss.MagVisible='on';
-optionss.PhaseMatching='on';
-optionss.PhaseMatchingValue=-180;
-optionss.PhaseMatchingFreq=1;
-optionss.Grid='on';
-
-
-
 Ts = 20e-3;
 
 k_planta = -0.099863;
@@ -27,28 +17,37 @@ D = 0;
 
 sys_fisc = ss(A, B, C, D);
 
-% x0 = [pocisiconincial; velocidad_incial]
-x2_0 = 0;
+% x0 = [pos_inicial; velocidad_incial]
 x1_0 = 5 * pi / (180);
+x2_0 = 0;
 x0 = [x1_0; x2_0];
 %initial(sys_fisc, x0);
 
 
 
 % ----- DISEÑO CONTROLADOR ----- %
-kc = -1 * db2mag(55);
-C = zpk([-12, -15],[0, -80],kc)
+kc = -1 * db2mag(56.5);
+C = zpk([-11, -12],[0, -80], kc)
 pid(C)
 
 L = minreal(C*P);
 
+%%
 
 % ----- GRAFICOS ----- %
-figure();
-bode(P,optionss);title("P")
+optionss=bodeoptions;
+optionss.MagVisible='on';
+optionss.PhaseMatching='on';
+optionss.PhaseMatchingValue=-180;
+optionss.PhaseMatchingFreq=1;
+optionss.Grid='on';
 
-figure();
-bode(C,optionss);title("C")
+
+%figure();
+%bode(P,optionss);title("P")
+
+%figure();
+%bode(C,optionss);title("C")
 
 figure();
 bode(L,optionss);title("L")
@@ -57,7 +56,7 @@ bode(L,optionss);title("L")
 % ----- CONTROL DIGITAL ----- %
 c_dig = c2d(C, Ts, 'tustin')
 
-pid = Kp + Ki * 1/s + Kd * s;
+%pid = Kp + Ki * 1/s + Kd * s;
 
 %0.004 * ki
 % con esta configuracion el controlador satura 
@@ -65,3 +64,12 @@ pid = Kp + Ki * 1/s + Kd * s;
 % derivatvo = 25 , integrak = 5, proporcional = 10
 
 % $$u[k] = u[k-1] - 694.94 e[k] + 567.00 e[k-1]$$
+
+%%
+k_s = pid(C)
+C_pid = (k_s.Kp - 300) + (k_s.Ki+300) /s + (k_s.Kd+3)*s/(k_s.Tf * s+1 )
+
+c_dig = c2d(C_pid, Ts, 'tustin')
+simu = pid(C_pid)
+
+% últ probado en clase (vídeo): %C_pid = (-316) + (-2100) /s + (0)*s/(k_s.Tf * s+1 ) 
